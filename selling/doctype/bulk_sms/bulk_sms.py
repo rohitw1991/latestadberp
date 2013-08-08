94d4007d485a1e932f56c38557990b1c7964e146
root <root@rohit-Aspire-5738.(none)>
2013-08-08 14:14:53 +0530

    rohit

--- selling/doctype/bulk_sms/bulk_sms.py
+++ selling/doctype/bulk_sms/bulk_sms.py
@@ -2,15 +2,7 @@
 
 from __future__ import unicode_literals
 import webnotes
-from webnotes import msgprint
-from selling.doctype.sms_center.sms_center import Doctype
 
-class DocType1(Doctype):
-	def __init__(self, doc, doclist=[]):
-		self.doc=doc 
-		self.doclist = doclist
-
-        def get_down(self,ur):
-		msgprint(ur)
-		
-
+class DocType:
+        def __init__(self, d, dl):
+                self.doc, self.doclist = d, dl
